614476116420181ff24fcf9bb03f0d9a75300189
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-10-24 22:46:28 -0500

    🟩 ElMapa_Debe_TenerVeintiunaPosiciones

--- Arañas/ArañasTest.cs
+++ Arañas/ArañasTest.cs
@@ -19,5 +19,5 @@ public class ArañasTest
 
 public class Mapa
 {
-    public List<int> Posiciones { get; set; }
+    public List<int> Posiciones { get; set; } = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 }
--- Arañas/TestList.md
+++ Arañas/TestList.md
@@ -1,4 +1,3 @@
 [] ElMapa_Debe_TenerVeintiunaPosiciones
-
  
  
